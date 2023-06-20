8295a2ba1a151cd10c8061ee6d6a04038348c223
LNQLinh <linh.lnq167944@gmail.com>
2023-06-20 16:44:33 +0700

    them Readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
-export type {ModalSelectDate} from "./ModalSelectDate"
+export  {ModalSelectDate} from "./ModalSelectDate"
